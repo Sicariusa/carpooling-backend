e7138118beedbea703a0cd2c3f45acc05acd13d3
Abdullah Mohamed <128763899+Sicariusa@users.noreply.github.com>
2025-04-10 16:05:33 +0200

    Remove dist folder from git tracking

--- services/ride-service/dist/src/middleware/auth.middleware.d.ts
+++
@@ -1,6 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-export declare class AuthMiddleware implements NestMiddleware {
-    private readonly logger;
-    use(req: Request, res: Response, next: NextFunction): Promise<void>;
-}
--- services/ride-service/dist/src/utils/kafka.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function connectConsumer(): Promise<void>;
-export declare function startConsumer(rideService: any): Promise<void>;
-export declare function disconnectConsumer(): Promise<void>;
-export declare function produceMessage(topic: string, message: any): Promise<boolean>;
